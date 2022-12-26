f2c9718f208dcc35e9cdbda99e83947f90dea666
Mellwvn <mellwvn@gmail.com>
2022-12-27 06:14:58 +0800

    First customization

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,35 +6,32 @@ st.set_page_config(layout="wide")
 st.sidebar.title("About")
 st.sidebar.info(
     """
-    Web App URL: <https://geospatial.streamlitapp.com>
-    GitHub repository: <https://github.com/giswqs/streamlit-geospatial>
+    Web App URL: <https://mellwvn-aziatek-streamlit-geospatial-streamlit-app-k2aufj.streamlit.app>
+    
     """
 )
 
 st.sidebar.title("Contact")
 st.sidebar.info(
     """
-    Qiusheng Wu: <https://wetlands.io>
-    [GitHub](https://github.com/giswqs) | [Twitter](https://twitter.com/giswqs) | [YouTube](https://www.youtube.com/c/QiushengWu) | [LinkedIn](https://www.linkedin.com/in/qiushengwu)
+    Mellwvn: <mellwvn.aziatek@gmail.com>
+    Izlan: <izlan.aziatek@gmail.com>
     """
 )
 
 # Customize page title
-st.title("Streamlit for Geospatial Applications")
+st.title("IX Geospatial Applications")
 
 st.markdown(
     """
-    This multipage app template demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and [leafmap](https://leafmap.org). It is an open-source project and you are very welcome to contribute to the [GitHub repository](https://github.com/giswqs/streamlit-multipage-template).
+    This multipage app template demonstrates various interactive web apps.
     """
 )
 
 st.header("Instructions")
 
 markdown = """
-1. For the [GitHub repository](https://github.com/giswqs/streamlit-multipage-template) or [use it as a template](https://github.com/giswqs/streamlit-multipage-template/generate) for your own project.
-2. Customize the sidebar by changing the sidebar text and logo in each Python files.
-3. Find your favorite emoji from https://emojipedia.org.
-4. Add a new app to the `pages/` directory with an emoji in the file name, e.g., `1_🚀_Chart.py`.
+Pages show the capabilities of this library, not actual data.
 
 """
 
